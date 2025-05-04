8a05167d6558653a19d19e090a69fbb4dc9515a8
h-isawa <29075851+navy1991@users.noreply.github.com>
2025-05-04 22:48:42 +0900

    add protobuf dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	id("com.google.protobuf") version "0.9.4"
+	id("java")
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -19,8 +21,12 @@ repositories {
 }
 
 dependencies {
+	implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.grpc:grpc-protobuf:1.63.0")
+	implementation("io.grpc:grpc-stub:1.63.0")
+	implementation("io.grpc:grpc-netty-shaded:1.63.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -29,6 +35,19 @@ dependencies {
 kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:3.25.1"
+	}
+	generateProtoTasks {
+		ofSourceSet("main").forEach {
+			it.builtins {
+				create("kotlin")
+			}
+		}
 	}
 }
 
