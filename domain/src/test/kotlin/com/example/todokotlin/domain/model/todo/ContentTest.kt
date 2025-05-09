8b8261603ce32772dbd48bbfa7bbcf7191a41360
h-isawa <29075851+navy1991@users.noreply.github.com>
2025-05-09 23:02:25 +0900

    add a test case for coverage

--- domain/src/test/kotlin/com/example/todokotlin/domain/model/todo/ContentTest.kt
+++ domain/src/test/kotlin/com/example/todokotlin/domain/model/todo/ContentTest.kt
@@ -1,12 +1,14 @@
 package com.example.todokotlin.domain.model.todo
 
+import com.example.todokotlin.domain.exception.TodoKotlinException
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ContentTest {
     @Test
-    @DisplayName("正常系：TODOの内容を生成")
+    @DisplayName("正常系：Todoの内容を生成")
     fun testFrom() {
         // Arrange
         val value = "あ".repeat(MAX_LENGTH)
@@ -16,6 +18,21 @@ class ContentTest {
 
         // Assert
         actual.value.shouldBe(value)
+    }
+
+    @Test
+    @DisplayName("異常系：Todoの内容を生成：最大文字数")
+    fun testFromErrorOverMaxLength() {
+        // Arrange
+        val value = "あ".repeat(MAX_LENGTH + 1)
+
+        // Act
+        val e = assertThrows<TodoKotlinException> { Content.from(value = value) }
+
+        // Assert
+        e.code.shouldBe(TodoKotlinException.Code.INVALID_CONTENT)
+        e.level.shouldBe(TodoKotlinException.Level.WARN)
+        e.message.shouldBe("The length of the content exceeds 50.")
     }
 
     companion object {
