b95679c4c27ef7f105559cb0f655f6491ef2518f
h-isawa <29075851+navy1991@users.noreply.github.com>
2025-05-21 09:47:00 +0900

    add springmockk

--- build-logic/src/main/kotlin/spring-boot-conventions.gradle.kts
+++ build-logic/src/main/kotlin/spring-boot-conventions.gradle.kts
@@ -6,7 +6,11 @@ plugins {
 dependencies {
     implementation(libs.findBundle("spring-boot-starter").get())
 
-    testImplementation(libs.findLibrary("spring-boot-starter-test").get())
+    testImplementation(libs.findLibrary("spring-boot-starter-test").get()) {
+        // https://github.com/Ninja-Squad/springmockk?tab=readme-ov-file#usage
+        exclude(module = "mockito-core")
+    }
+    testImplementation(libs.findLibrary("springmockk").get())
 }
 
 tasks.withType<Test> {
